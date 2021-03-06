54a2aeeda72faaa3824aea03dc399c68d38e63cd
@hyp3rstrike <hyp3rstrike@gmail.com>
2021-03-06 19:48:15 +1030

    Adding to dev

--- index.php
+++ index.php
@@ -2,23 +2,37 @@
     // Required Variables
     $svcScope = "https://www.googleapis.com/auth/youtube.readonly";
     $authFile = "auth/client_secret.json";
-    $oauthRedirect = "/";
+    $version = "2.0.0 [Stupid-Dick-Fuck]";
 
     // Load the Composer library
     require "vendor/autoload.php";
 
-    // Google OAuth Begins
+    // Google OAuth Begins *dramatic music*
     $client = new Google_Client();
     $client->setAuthConfig($authFile);
     $client->addScope($svcScope);
-    $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . $oauthRedirect);
+    $client->setRedirectUri('https://' . $_SERVER['HTTP_HOST']);
 
     // Begin User Prompt
     $authUrl = $client->createAuthUrl();
     $revoke = $client->revokeToken();
-    echo '<a href="' . $authUrl . '" class="authButton">Auth</a> | <a href"' . $revoke . '">Revoke</a>';
 
-    // Exchange code for auth token and do the request.
+    echo '
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <img src="inc/ytchat-logo.png" class="img-fluid mx-auto" alt="..." width="100px">
+        <h3 class="text-center">YouTube Chat Loader</h3>
+        <p class="text-center">Click on Authenticate to fetch your next stream chat room.</p>
+    </div>
+    <div class="d-grid gap-2 col-6 mx-auto buttons console">
+        <a href="' . $authUrl . '" class="btn btn-success" type="button">Authenticate</button></a>
+        <button class="btn btn-danger" type="button" disabled>Revoke Access</button>
+    </div>
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <p class="text-center">Created by <a href="https://twitter.com/hyp3rstrike">@hyp3rstrike</a> | Version ' . $version . '</p>
+    </div>
+    ';
+
+    // Exchange code for auth token and do the thing.
     if (isset($_GET['code'])) {
         $authtoken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
         $client->setAccessToken($authtoken);
@@ -33,6 +47,7 @@
         header('Location: https://www.youtube.com/live_chat?v=' . $streamId . '&is_popout=1&dark_theme=1');
         
     } else {
+        // or tell you something fucked up
         echo '<script>console.log("Either something is munted, or you didn\'t actually log in.");</script>';
     }
 
@@ -40,12 +55,12 @@
 
 <html>
     <head>
-        <title>YouTube Chat Loader v2.0 by @hyp3rstrike</title>
+        <title>YouTube Chat Loader by @hyp3rstrike</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <link rel="stylesheet" href="inc/main.css"></script>
         <script src="inc/main.js" type="text/javascript"></script>
     </head>
     <body>
-
+        <!-- Nothing required here because it's handled by the echo in PHP biatch. -->
     </body>
 </html>
